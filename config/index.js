a18f6df46439dbf1e4c9ed9d191815580f81152e
ali0613 <80727105+ali0613@users.noreply.github.com>
2022-09-15 15:05:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oBN1c5j9VuQOzaQEzJDLftTMSbRw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "m8Jd3hjP5FZ9KWqbbeU9f9KtOds2nvnPo98Xi0E4pGM",
+      useTemplateId: "x-MaX5wEnx0piWjhODKDdFZ0CJu8MMqL4EVqzy5KBow",
       // 所在省份
       province: "河南",
       // 所在城市
@@ -41,16 +41,12 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+        {"type": "*生日", "name": "悦悦和小卢", "year": "2002", "date": "06-13"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "birthday_message", date: "2002-06-13"},
+        {"keyword": "love_day", date: "2019-04-05"},
         // 结婚纪念日
         {"keyword": "marry_day", date: "2022-09-09"},
         // 退伍日
